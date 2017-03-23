3739c8b03bb383b66bb41c815231a45ffb225128
aZerato <aZerato.graphe@gmail.com>
2017-03-23 11:12:15 +0100

    UoW implementation

---
+++ MyApp/MyApp.Data.Core/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿namespace MyApp.Data.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IUnitOfWork : IDisposable
+    {
+        void Commit();
+
+        void CommitAndRefreshChanges();
+
+        void RollbackChanges();
+    }
+}
---
+++ MyApp/MyApp.Data/UnitOfWorkContext.cs
@@ -0,0 +1,77 @@
+﻿namespace MyApp.Data
+{
+    using Core;
+    using Domain.SampleModule.Aggregates;
+    using System.Data;
+    using System.Data.Entity;
+    using System.Linq;
+
+    /// <summary>
+    /// EF context for MyApp Database.
+    /// </summary>
+    public class UnitOfWorkContext : DbContext, IUnitOfWork
+    {
+        #region ---- Constructor ----
+
+        /// <summary>
+        /// Default Constructor.
+        /// </summary>
+        public UnitOfWorkContext()
+            : base("MyAppConnectionString")
+        {
+
+        }
+
+        #endregion
+
+        #region ---- properties ----
+        
+        public IDbSet<SampleData> SampleDatas { get; set; }
+
+        #endregion
+
+        #region ---- UnitOfWork Implementation ----
+
+        /// <summary>
+        /// <see cref="IUnitOfWork.Commit"/>
+        /// </summary>
+        public void Commit()
+        {
+            this.SaveChanges();
+        }
+
+        /// <summary>
+        /// <see cref="IUnitOfWork.CommitAndRefreshChanges"/>
+        /// </summary>
+        public void CommitAndRefreshChanges()
+        {
+            // Change all Entities in change tracker as 'unchanged' state
+            base.ChangeTracker.Entries()
+                              .ToList()
+                              .ForEach(entry => entry.State = EntityState.Unchanged);
+        }
+
+        /// <summary>
+        /// <see cref="IUnitOfWork.RollbackChanges"/>
+        /// </summary>
+        public void RollbackChanges()
+        {
+            var valid = false;
+
+            while (valid)
+            {
+                try
+                {
+                    this.SaveChanges();
+                    valid = true;
+                }
+                catch (DBConcurrencyException)
+                {
+                    valid = false;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
